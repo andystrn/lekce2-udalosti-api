b6380e7d36e2bb31626cbdc3851c6eb05bb426fb
Andrea Strnadlova <an.strnadlova@gmail.com>
2022-02-20 09:49:42 +0100

    using the classList and add/remove class function

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // tady je místo pro náš program
+
+console.log("Ahoj")
+
+let nadpis = document.querySelector('h1');
+nadpis.classList.add('zluty');
+nadpis.classList.remove('zluty');
+
+let ctverecek = document.querySelector('div.zluty');
+ctverecek.classList.add('ctverecek');
